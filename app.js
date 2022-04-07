205162c13412dc9343ba63dc8144683f98601b4a
Shriprasanna B <shriprasanna32@gmail.com>
2022-04-07 21:13:01 +0530

    add comments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//selectors
 const allInputs = document.querySelectorAll("input");
 const btn = document.querySelector("button");
 
+//documentElement used to pick the :root thing from "css"
 function changeVar() {
   const unit = this.dataset.size || "";
   document.documentElement.style.setProperty(
@@ -11,8 +13,10 @@ function changeVar() {
 
 allInputs.forEach((inp) => {
   inp.addEventListener("input", changeVar);
+  //input event used to get a value from slider
 });
 
+//just for random image 
 let i = 1;
 btn.addEventListener("click", () => {
   if (3 >= i) {
